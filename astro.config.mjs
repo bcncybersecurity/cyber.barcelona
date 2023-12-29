a294e56c7f49bbfc3b0d7f37e6ee3c7ec8c4b9b7
ivanol55 <ivan.mendez@factorial.co>
2023-12-29 13:08:25 +0100

    lang patches

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://cyber.barcelona',
+  integrations: [
+    tailwind(), 
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          en: 'en-US',
+          es: 'es-ES',
+          fr: 'fr-CA'
+        },
+      },
+    }),
+  ]
 });
