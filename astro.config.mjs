f743a03c2df6b4f6a883c112a8c6a19ad805d2a0
ivanol55 <ivan.mendez@factorial.co>
2023-12-29 16:53:08 +0100

    Add basic Cookie Consent

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,8 +16,9 @@ export default defineConfig({
           en: 'en-US',
           es: 'es-ES',
           fr: 'fr-CA'
-        },
-      },
-    }),
+        }
+      }
+    }), 
+    spotlightjs()
   ]
 });
