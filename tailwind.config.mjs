78c394fa86a446e2c4e63b5ead59e7b3592f3e1b
narmanguebraun <narmanguebraun1@gmail.com>
2024-05-13 23:33:28 +0200

    new design proposal

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#FFFFFF",
+      brand: "#827edc",
+      darkblue: "#26273B",
+      navy: "#232441",
+      indigo: "#465FFF",
+      gray: "#6F708B",
+      gray01: "#F0F0F5",
+      gray02: "#D6D6E1",
+      gray03: "#ACADC1",
+      gray04: "#6F708B",
+      gray05: "#454560",
+      gray06: "#26273B",
+      dark: "#181820",
+    },
+    fontFamily: {
+      sans: ["Inter", "Helvetica", "sans-serif"],
+      mono: ["JetBrains Mono", "Courier", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
