50b33dfed8b0e0daf63ddb001fccd12b991d6e2f
narmanguebraun <narmanguebraun1@gmail.com>
2024-05-15 22:58:36 +0200

    Refactor events

---
+++ src/data/events.ts
@@ -0,0 +1,97 @@
+export const events = [
+  {
+    id: 1,
+    title: "My journey into privacy",
+    url: "https://www.meetup.com/es/barcelona-cybersecurity/events/288976517/",
+    imgSrc: "/img/events/journey-into-privacy.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+  {
+    id: 2,
+    title: "¿Necesitaría Barcelona una figura de CISO?",
+    url: "https://www.meetup.com/es/barcelona-cybersecurity/events/289561917/",
+    imgSrc: "/img/events/barcelona-ciso.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+  {
+    id: 3,
+    title: "Honeypots, caer en la trampa",
+    url: "https://www.meetup.com/es/barcelona-cybersecurity/events/290696272/",
+    imgSrc: "/img/events/honeypots.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+  {
+    id: 4,
+    title: "Security in SaaS platforms",
+    url: "https://www.meetup.com/es-ES/barcelona-cybersecurity/events/291682269/",
+    imgSrc: "/img/events/security-in-saas.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+  {
+    id: 5,
+    title:
+      "Zero Trust: A fundamental catalyst for a successful cyber resilience strategy",
+    url: "https://www.meetup.com/es/barcelona-cybersecurity/events/292431804/",
+    imgSrc: "/img/events/zero-trust.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+  {
+    id: 6,
+    title: "AI4CYBER: AI aplicada a ciberseguridad",
+    url: "https://www.meetup.com/es-ES/barcelona-cybersecurity/events/293146261/",
+    imgSrc: "/img/events/ai4cyber.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+  {
+    id: 7,
+    title: "Barcelona Cybersecurity's Afterwork: Opening the new season",
+    url: "https://www.meetup.com/es-ES/barcelona-cybersecurity/events/295583936/",
+    imgSrc: "/img/events/barcelona-cyber-afterwork.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+  {
+    id: 8,
+    title: "Trust, but verify, o porque Ronal Reagan no sabía de Zero Trust",
+    url: "https://www.meetup.com/es-ES/barcelona-cybersecurity/events/296121768/",
+    imgSrc: "/img/events/trust-but-verify.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+  {
+    id: 9,
+    title:
+      "Seguridad de altura: un caso de ciberseguridad OT en transporte aéreo",
+    url: "https://www.meetup.com/es-ES/barcelona-cybersecurity/events/296833601/",
+    imgSrc: "/img/events/seguridad-de-altura.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+  {
+    id: 10,
+    title: "Explotaciones en Red Team, no todo es sota, caballo y rey.",
+    url: "https://www.meetup.com/es-ES/barcelona-cybersecurity/events/297277260/",
+    imgSrc: "/img/events/red-team-sota-caballo-rey.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+  {
+    id: 11,
+    title: "Barcelona Cybersecurity Opening Event 2024",
+    url: "https://www.meetup.com/es-ES/barcelona-cybersecurity/events/298549063/",
+    imgSrc: "/img/events/opening-event-2024.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+  {
+    id: 12,
+    title:
+      "De Dev a Devastación: Explotación de Flujos CICD para Infiltración y Persistencia",
+    url: "https://www.meetup.com/es-ES/barcelona-cybersecurity/events/299273534/",
+    imgSrc: "/img/events/de-dev-a-devastacion.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+  {
+    id: 13,
+    title:
+      "MobileApp Security: Everything you ever wanted to know and were too afraid to ask",
+    url: "https://www.meetup.com/es-ES/barcelona-cybersecurity/events/299766501/",
+    imgSrc: "/img/events/mobile-app-security.webp",
+    imgAltText: "Barcelona cybersecurity event photo",
+  },
+];
